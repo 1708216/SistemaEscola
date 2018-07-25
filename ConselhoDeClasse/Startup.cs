46f191f4aec9a313707a41486fe6b1aeb0cb8a34
MARCELO <marcelofabiano@msn.com>
2018-07-25 17:04:12 -0300

    Add project files.

---
+++ ConselhoDeClasse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConselhoDeClasse.Startup))]
+namespace ConselhoDeClasse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
